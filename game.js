54c1c204c51ddba21dc534f1d7c3ea6b60356a59
Marko Knöbl <marko.kn@gmail.com>
2016-10-15 23:49:35 +0200

    make "clearFullRows" a method of TetrisGame

--- game.js
+++ game.js
@@ -13,3 +13,33 @@ function TetrisGame(config) {
 TetrisGame.prototype.canMoveTo = function(shape) {
   return shape.isInsideField(this.fieldSize) && !shape.overlapsAny(this.existingBlocks);
 };
+
+TetrisGame.prototype.clearFullRows = function(existingBlocks, fieldSize) {
+  // removes complete rows from the model
+  var counts = [];
+  for (var i = 0; i < this.fieldSize[1]; i ++) {
+    counts.push(0);
+  }
+  this.existingBlocks.forEach(function(block) {
+    counts[block.position[1]] ++;
+  });
+  for (var i = 0; i < this.fieldSize[1]; i ++) {
+    if (counts[i] === this.fieldSize[0]) {
+      // there's a full row
+      var newExistingBlocks = []
+      // copy all but the full row
+      this.existingBlocks.forEach(function(block, index) {
+        if (block.position[1] > i) {
+          // keep rows below full row the same
+          newExistingBlocks.push(block);
+        } else if (block.position[1] < i) {
+          // move rows above full row further down
+          block.position[1] ++;
+          newExistingBlocks.push(block);
+        }
+      });
+      // overwrite the original
+      this.existingBlocks = newExistingBlocks;
+    }
+  }
+}
